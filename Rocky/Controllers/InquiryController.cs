36c6b35b88347df9b3e29b3b826471cd91a76c44
Dastan Nasirdinov <dastan.nasirdinov@gmail.com>
2022-10-21 12:18:47 +0600

    Added InquiryController and InquiryVM

---
+++ Rocky/Controllers/InquiryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Rocky_DataAccess;
+using Rocky_DataAccess.Repository.IRepository;
+using Rocky_Models;
+using Rocky_Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class InquiryController : Controller
+    {
+        private readonly IInquiryHeaderRepository _inqHRepo;
+        private readonly IInquiryDetailRepository _inqDRepo;
+
+        public InquiryController(IInquiryHeaderRepository inqHRepo, IInquiryDetailRepository inqDRepo)
+        {
+            _inqHRepo = inqHRepo;
+            _inqDRepo = inqDRepo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
