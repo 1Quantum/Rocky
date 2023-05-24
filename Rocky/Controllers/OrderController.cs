f4898d628875db4e2fd9a0352302c1e7c4bed543
Dastan Nasirdinov <dastan.nasirdinov@gmail.com>
2023-05-24 17:57:45 +0600

    Skipped section 8, Section 9 half is done

---
+++ Rocky/Controllers/OrderController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky_DataAccess.Repository.IRepository;
+using Rocky_Models.ViewModels;
+using Rocky_Utility;
+using Rocky_Utility.BrainTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderHeaderRepository _orderHRepo;
+        private readonly IOrderDetailRepository _orderDRepo;
+        private readonly IBrainTreeGate _brain;
+
+
+
+        public OrderController(IOrderHeaderRepository orderHRepo, IOrderDetailRepository orderDRepo, IBrainTreeGate brain)
+        {
+            _orderHRepo = orderHRepo;
+            _orderDRepo = orderDRepo;
+            _brain = brain;
+        }
+        public IActionResult Index()
+        {
+            OrderListVM orderListVM = new OrderListVM()
+            {
+                OrderHList = _orderHRepo.GetAll(),
+                StatusList = WC.listStatus.ToList().Select(x => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem { Text = x, Value = x })
+            };
+
+            return View(orderListVM);
+        }
+    }
+}
